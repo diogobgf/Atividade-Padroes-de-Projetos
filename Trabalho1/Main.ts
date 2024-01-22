f4cebac8dfe5a291bbe0c9289eedd1637a0087e2
diogobgf <116925404+diogobgf@users.noreply.github.com>
2024-01-22 19:03:14 +0000

    v1.0

--- Trabalho1/Main.ts
+++ Trabalho1/Main.ts
@@ -1,13 +1,10 @@
 import { Fabrica } from "./Fabrica";
 
 
-const computadorPc = Fabrica.criarComputador(4, 1048, 2.5, "computador");
+const computadorPC = Fabrica.criarComputador(4, 1048, 2.5, "computador");
 const computadorServidor = Fabrica.criarComputador(24, 2097, 5.2, "servidor");
 
-
-console.log(computadorPc.toString());
-console.log();
-console.log(computadorServidor.toString());
-console.log();
+console.log(computadorPC?.toString());
+console.log(computadorServidor?.toString());
 
 const  na = Fabrica.criarComputador(0, 0, 0, "Na");
